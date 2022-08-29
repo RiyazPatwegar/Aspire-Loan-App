2fd4cdcbcba9df545ad29486f65279afd7975dba
Riyaz Patwegar <riyaz.patwegar@jetsynthesys.com>
2022-08-29 12:44:54 +0530

    feature test cases

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -12,17 +12,17 @@ class CustomerTest extends TestCase
      *
      * @return void
      */
-  
+
     public function test_apply_customer_loan()
     {
-        $response = $this->post('/api/v1/customer-loan/apply', ["customerId" => 20, "amount" => 10000, "term" => 3]);
+        $response = $this->post('/api/v1/customer-loan/apply', ["customerId" => 1, "amount" => 10000, "term" => 3]);
 
         $response->assertStatus(200);
     }
 
     /* public function test_customer_get_loan_status()
     {
-        $response = $this->get('/api/v1/customer-loan/getLoanStatus?customerId=117&viewPolicyCheck=true');
+        $response = $this->get('/api/v1/customer-loan/getLoanStatus?customerId=1&viewPolicyCheck=true');
         $response->assertStatus(200);
     } */
 
@@ -35,14 +35,14 @@ class CustomerTest extends TestCase
 
     public function test_admin_approve_loan()
     {
-        $response = $this->post('/api/v1/admin/approveLoan', ["applicationId" => 19, "adminId"=> 1]);
+        $response = $this->post('/api/v1/admin/approveLoan', ["applicationId" => 1, "adminId"=> 1]);
 
         $response->assertStatus(200);
     }
 
     public function test_payment_pay()
     {
-        $response = $this->post('/api/v1/payment/payNow', ["applicationId" => 19, "customerId"=> 20, "amount"=> 3333.34]);
+        $response = $this->post('/api/v1/payment/payNow', ["applicationId" => 1, "customerId"=> 1, "amount"=> 3333.34]);
 
         $response->assertStatus(200);
     }
